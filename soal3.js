8080584bc49cc268432b0cef73b18b356621122d
fitradev <fietra.11@gmail.com>
2018-06-03 21:07:12 +0700

    soal3

---
+++ soal3.js
@@ -0,0 +1,9 @@
+function digitsGrouper(numbers) {
+  // only code here..
+}
+
+console.log(digitsGrouper([1, 12, 13, 0, 88, 123, 456])); // [ [ 1, 0 ], [ 12, 13, 88 ], [ 123, 456 ], [] ]
+console.log(digitsGrouper([1234, 8, 2, 543, 1, 11, 222, 7654])); // [ [ 8, 2, 1 ], [ 11 ], [ 543, 222 ], [ 1234, 7654 ] ]
+console.log(digitsGrouper([1111, 1, 6, 5, 5, 555, 111, 11, 55, 5555])); // [ [ 1, 6, 5, 5 ], [ 11, 55 ], [ 555, 111 ], [ 1111, 5555 ] ]
+console.log(digitsGrouper([])); // [ [], [], [], [] ]
+console.log(digitsGrouper([0, 00, 000, 0000])); // [ [ 0, 0, 0, 0 ], [], [], [] ]
